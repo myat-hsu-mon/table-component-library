4126f5f680a696235d395405e138f525caac143c
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2024-01-29 17:25:47 +0630

    Update configs and package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        yellow: {
+          light: "#FFF7E6",
+          DEFAULT: "#F2994A",
+          dark: "#FF9900",
+        },
+        green: {
+          light: "#EBF9F5",
+          DEFAULT: "#219653",
+          dark: "#3EAA86",
+        },
+        red: {
+          light: "#FDF4F7",
+          DEFAULT: "#EB5757",
+          dark: "#B93E5C",
+        },
+        blue: {
+          DEFAULT: "#0F6FDE",
+        },
+        gray: {
+          light: "#F8FAFB",
+          DEFAULT: "#4A5D75",
+          dark: "#091626",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
-
