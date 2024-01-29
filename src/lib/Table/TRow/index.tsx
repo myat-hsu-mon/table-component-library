4e35e758fcc4815542177ea2d3237200600686b2
myat-hsu-mon <47687494+myat-hsu-mon@users.noreply.github.com>
2024-01-29 17:33:26 +0630

    feat: THead component

---
+++ src/lib/Table/TRow/index.tsx
@@ -0,0 +1,48 @@
+import Checkbox from "../../../components/Checkbox";
+import Department from "../../../components/Department";
+import FormatDate from "../../../components/FormatDate";
+import Profile from "../../../components/Profile";
+import Status from "../../../components/Status";
+import { formatUSCurrency, getFormattedDate } from "../../../utils";
+
+import { Employee } from "../../../interfaces";
+
+interface TRowProps {
+  data: Employee;
+  checked: boolean;
+  onChange: (rowId: number) => void;
+}
+
+export default function TRow({ data, checked, onChange }: TRowProps) {
+  const formattedDate = getFormattedDate(data.paymentDate);
+
+  return (
+    <tr
+    // className={classNames({
+    //   "bg-gray-light": data.department === "sales",
+    // })}
+    >
+      <td className="whitespace-nowrap py-4 pl-4 pr-2 text-center text-sm">
+        <Checkbox checked={checked} onChange={() => onChange(data.id)} />
+      </td>
+      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-0">
+        <Profile name={data.employeeName} key={data.employeeName} />
+      </td>
+      <td className="whitespace-nowrap py-4 pr-3">
+        <Department department={data.department} />
+      </td>
+      <td className="whitespace-nowrap py-4 pr-3 text-sm text-gray-dark">
+        {formatUSCurrency(data.salary)}
+      </td>
+      <td className="whitespace-nowrap py-4 pr-3 text-sm text-gray-dark">
+        <FormatDate formattedDate={formattedDate} />
+      </td>
+      <td className="whitespace-nowrap py-4 pr-3 text-sm text-gray-dark">
+        <Status status={data.paymentStatus} />
+      </td>
+      <td className="whitespace-nowrap py-4 pr-3 text-sm text-gray-dark">
+        {data.details}
+      </td>
+    </tr>
+  );
+}
